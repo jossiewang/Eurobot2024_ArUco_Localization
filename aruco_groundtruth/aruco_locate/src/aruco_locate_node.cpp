cefef820b2f7f2d0bde44e674384a66145bc5719
jossiewang <jossiew621@gapp.nthu.edu.tw>
2024-02-04 21:42:45 +0800

    aruco_locate_node done, add aruco marker code M123 and M456 (now will be M27 and M28) to dictionary

--- aruco_groundtruth/aruco_locate/src/aruco_locate_node.cpp
+++ aruco_groundtruth/aruco_locate/src/aruco_locate_node.cpp
@@ -3,8 +3,10 @@
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
-// #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/Quaternion.h>
+#include <eigen3/Eigen/Dense>
 
 int main(int argc, char** argv) {
     try {
@@ -14,18 +16,22 @@ int main(int argc, char** argv) {
         ros::Time::setNow(ros::Time::now());
         tf2_ros::Buffer tf_buffer;
         tf2_ros::TransformListener tf_Listener(tf_buffer);
-        ros::Publisher pub = node.advertise<geometry_msgs::PoseStamped>("pos_aruco", 10); //original 1000, can change to 10??
+        ros::Publisher pub = node.advertise<geometry_msgs::PoseStamped>("pose_aruco", 10); //original 1000, can change to 10??
         ros::Rate rate(10.0);
+
+        int map_num = 0;
 
         while (node.ok()){
             
             //get four maps
+            map_num = 0;
             //lookup Mhead to map_avg
             geometry_msgs::TransformStamped map_Mhead;
             try{
-                if(tf_buffer.canTransform("map_avg", "Mhead", ros::Time::now())){
-                    map_Mhead = tf_buffer.lookupTransform("map_avg", "Mhead",ros::Time(0));
-                    ROS_INFO("canTransform20, [%f, %f, %f]", map_Mhead.transform.translation.x, map_Mhead.transform.translation.y, map_Mhead.transform.translation.z);
+                if(tf_buffer.canTransform("map_avg", "M27", ros::Time::now())){
+                    map_Mhead = tf_buffer.lookupTransform("map_avg", "M27",ros::Time(0));
+                    // ROS_INFO("canTransform20, [%f, %f, %f]", map_Mhead.transform.translation.x, map_Mhead.transform.translation.y, map_Mhead.transform.translation.z);
+                    map_num++;
                 }
             }
             catch (tf2::TransformException &ex) {
@@ -33,36 +39,56 @@ int main(int argc, char** argv) {
                 ros::Duration(1.0).sleep();
                 continue;
             }
-            // //lookup Mtail to map_avg
-            // geometry_msgs::TransformStamped map_Mtail;
-            // try{
-            //     if(tf_buffer.canTransform("map_avg", "Mtail", ros::Time::now())){
-            //         map_Mtail = tf_buffer.lookupTransform("map_avg", "Mtail",ros::Time(0));
-            //         ROS_INFO("canTransform20, [%f, %f, %f]", map_Mtail.transform.translation.x, map_Mtail.transform.translation.y, map_Mtail.transform.translation.z);
-            //     }
-            // }
-            // catch (tf2::TransformException &ex) {
-            //     ROS_WARN("%s",ex.what());
-            //     ros::Duration(1.0).sleep();
-            //     continue;
-            // }
+            //lookup Mtail to map_avg
+            geometry_msgs::TransformStamped map_Mtail;
+            try{
+                if(tf_buffer.canTransform("map_avg", "M28", ros::Time::now())){
+                    map_Mtail = tf_buffer.lookupTransform("map_avg", "M28",ros::Time(0));
+                    // ROS_INFO("canTransform20, [%f, %f, %f]", map_Mtail.transform.translation.x, map_Mtail.transform.translation.y, map_Mtail.transform.translation.z);
+                    map_num++;
+                }
+            }
+            catch (tf2::TransformException &ex) {
+                ROS_WARN("%s",ex.what());
+                ros::Duration(1.0).sleep();
+                continue;
+            }
 
             // take average to get pose (but the markers are too large! maybe one is enough?)
+                //nope orientation of aruco isn't reliable for camera on top...
 
-            geometry_msgs::PoseStamped robot_head;
-            // geometry_msgs::PoseStamped robot_tail;
-            
-            robot_head.header.frame_id = "map_avg";
-            robot_head.header.stamp = ros::Time::now();
-            robot_head.pose.position.x = map_Mhead.transform.translation.x;
-            robot_head.pose.position.y = map_Mhead.transform.translation.y;
-            robot_head.pose.position.z = map_Mhead.transform.translation.z;
-            robot_head.pose.orientation.x = map_Mhead.transform.rotation.x;
-            robot_head.pose.orientation.y = map_Mhead.transform.rotation.y;
-            robot_head.pose.orientation.z = map_Mhead.transform.rotation.z;
-            robot_head.pose.orientation.w = map_Mhead.transform.rotation.w;
+            if(map_num!=2) continue;
 
-            pub.publish(robot_head);
+            //translation average
+            geometry_msgs::TransformStamped avg_transform;
+            avg_transform.transform.translation.x = (map_Mhead.transform.translation.x + map_Mtail.transform.translation.x)/map_num;
+            avg_transform.transform.translation.y = (map_Mhead.transform.translation.y + map_Mtail.transform.translation.y)/map_num;
+            avg_transform.transform.translation.z = (map_Mhead.transform.translation.z + map_Mtail.transform.translation.z)/map_num;
+
+            //get rotation
+            geometry_msgs::Vector3 ori_vec;
+            ori_vec.x = map_Mhead.transform.translation.x - map_Mtail.transform.translation.x; 
+            ori_vec.y = map_Mhead.transform.translation.y - map_Mtail.transform.translation.y; 
+            ori_vec.z = map_Mhead.transform.translation.z - map_Mtail.transform.translation.z; 
+
+            // Convert Vector3 to Quaternion using Eigen -> TODO: test
+            Eigen::Vector3d eigen_vector(ori_vec.x, ori_vec.y, ori_vec.z);
+            Eigen::Quaterniond quaternion = Eigen::Quaterniond::FromTwoVectors(Eigen::Vector3d::UnitX(), eigen_vector.normalized());
+            // ROS_INFO("q: %7f, %7f, %7f, %7f", quaternion.x(), quaternion.y(), quaternion.z(), quaternion.w());
+
+            geometry_msgs::PoseStamped robot_tummy;
+
+            robot_tummy.header.frame_id = "map_avg";
+            robot_tummy.header.stamp = map_Mhead.header.stamp;
+            robot_tummy.pose.position.x = avg_transform.transform.translation.x;
+            robot_tummy.pose.position.y = avg_transform.transform.translation.y;
+            robot_tummy.pose.position.z = avg_transform.transform.translation.z;
+            robot_tummy.pose.orientation.x = quaternion.x();
+            robot_tummy.pose.orientation.y = quaternion.y();
+            robot_tummy.pose.orientation.z = quaternion.z();
+            robot_tummy.pose.orientation.w = quaternion.w();
+
+            pub.publish(robot_tummy);
 
         }
 
